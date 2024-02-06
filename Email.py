bdb6ff5981dcf8dd94ab9fe4ed74672ef9222edf
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-02-06 09:31:13 +0100

    anpassen email empfänger; keine For schleife für 3 emails (alle in einer)

--- Email.py
+++ Email.py
@@ -14,30 +14,30 @@ def send_email():
 
     #receivers = {'michael.pacher@emea-cosmetics.com': 'Michael','stefan.kandler@emea-cosmetics.com': 'Stefan','martin.bergler@emea-cosmetics.com': 'Martin',
                      #'josef.geisberger@emea-cosmetics.com': 'Josef', 'christoph.razek@emea-cosmetics.com': 'Chris', 'rainer.kienle@emea-cosmetics.com': 'Rainer'}
-    receivers = {'stefan.kandler@emea-cosmetics.com': 'Stefan','christoph.razek@emea-cosmetics.com' : 'Chris', 'martin.bergler@emea-cosmetics.com' : 'Martin'}
+    receivers = ['stefan.kandler@emea-cosmetics.com','christoph.razek@emea-cosmetics.com','martin.bergler@emea-cosmetics.com']
 
     today =date.today()
 
-    for r in receivers:
-        text = f"""Hallo {receivers[r]},\n\nAnbei findest du die wöchentliche Bestandsauswertung.\nBei Fragen einfach melden\nMit freundlichen Grüßen\n\nChris"""
-        mail_item = olApp.CreateItem(0)
-        mail_item.Subject = f'Lagerbestände am: {today}'
-        mail_item.BodyFormat = 1
-        mail_item.Body = text
 
-        mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\LagerbestandV2.xlsx')
-        mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Lagerwert_KEK.xlsx')
-        mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Lagerwert_VK.xlsx')
-        #mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Pivot_KEK.png')
-        #mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Pivot_VK.png')
+    text = f"""Hallo \n\nAnbei findest du die wöchentliche Bestandsauswertung.\nBei Fragen einfach melden\nMit freundlichen Grüßen\n\nChris"""
+    mail_item = olApp.CreateItem(0)
+    mail_item.Subject = f'Lagerbestände am: {today}'
+    mail_item.BodyFormat = 1
+    mail_item.Body = text
 
-        mail_item.Sender = 'christoph.razek@emea-cosmetics.com'
-        mail_item.To = r
+    mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\LagerbestandV2.xlsx')
+    mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Lagerwert_KEK.xlsx')
+    mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Lagerwert_VK.xlsx')
+    #mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Pivot_KEK.png')
+    #mail_item.Attachments.Add(r'S:\EMEA\Lagerbestand\Pivot_VK.png')
+
+    mail_item.Sender = 'christoph.razek@emea-cosmetics.com'
+    mail_item.To = ";".join(receivers)
 
 
-        mail_item.Display()
-        mail_item.Save()
-        #mail_item.Send()
+    mail_item.Display()
+    mail_item.Save()
+    #mail_item.Send()
     #shutil.copy(r'S:\EMEA\Lagerbestand\LagerbestandV2.xlsx',fr'S:\EMEA\Lagerbestand\Wochenauswertung\Lagerbestand_{today}.xlsx')
 
 
